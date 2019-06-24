12207ff78f08c1efdfb23991c0504b51861e6ee9
SyedAkhter <dev.syed.akhter@gmail.com>
2019-06-24 15:54:09 -0400

    2nd commit

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require("express");
-const mysql = require("mysql");
-const bodyParser = require("body-parser");
-const userRouter = require("./api/routes/userRoutes");
-const ticketRouter = require("./api/routes/ticketRoutes");
-const organizationRouter = require("./api/routes/organizationRoute");
-const authRoute = require("./api/routes/authRoute");
-const utils = require("./api/utils/auth");
-const cors = require("cors");
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const userRouter = require('./api/routes/userRoutes');
+const ticketRouter = require('./api/routes/ticketRoutes');
+const organizationRouter = require('./api/routes/organizationRoute');
+const authRoute = require('./api/routes/authRoute');
+const utils = require('./api/utils/auth');
+const cors = require('cors');
 const app = express();
-const db = require("./config/db");
-port = 3000;
+const db = require('./config/db');
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // testing for connection
 db.authenticate()
   .then(function() {
-    console.log("database  is connected successfully");
+    console.log('database  is connected successfully');
   })
   .catch(function(err) {
-    console.log("wrong");
+    console.log('wrong');
   })
   .done();
 
-const path = require("path");
-app.use(express.static(__dirname + "/build"));
+const path = require('path');
+app.use(express.static(__dirname + '/build'));
 
 app.use(authRoute);
 
@@ -34,5 +34,5 @@ app.use(userRouter);
 app.use(ticketRouter);
 app.use(organizationRouter);
 app.listen(port, () => {
-  console.log("listing at port ", port);
+  console.log('listing at port ', port);
 });
