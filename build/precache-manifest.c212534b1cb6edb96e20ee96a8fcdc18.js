ca2412a4cfddd474b7e20cdd354130586ec0a802
SyedAkhter <dev.syed.akhter@gmail.com>
2019-06-24 15:07:16 -0400

    first commit

---
+++ build/precache-manifest.c212534b1cb6edb96e20ee96a8fcdc18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c296bc1f046f0a2d85a0c540bcf05c57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "330f98e7c13c5c6399e7",
+    "url": "/static/css/2.a6ea2dfd.chunk.css"
+  },
+  {
+    "revision": "b80dff240043a66ffb6b",
+    "url": "/static/css/main.0fba1c82.chunk.css"
+  },
+  {
+    "revision": "330f98e7c13c5c6399e7",
+    "url": "/static/js/2.62996c54.chunk.js"
+  },
+  {
+    "revision": "b80dff240043a66ffb6b",
+    "url": "/static/js/main.8210a209.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
